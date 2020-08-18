665aa6f1f3f295c321d1d086685f5bffe4b82afa
allgoodmans571 <allgoodmans571@gmail.com>
2020-08-18 19:49:59 +0300

    no message

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-let express = require("express")
+var express = require('express');
+var app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.post("/getData", function (req, res) {
+
+
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/html/index.html');
+});
+
+console.log("Сервер запущен...")
+app.listen(80);
