8a00bb80901561bd383a7945b685cad1b7237fca
Alex <allgoodmans571@gmail.com>
2021-04-09 06:35:49 +0300

    fixin

--- index.js
+++ index.js
@@ -1,53 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
-const csv = require('csv-parser');
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+const csv = require("csv-parser");
 
-
-const createCsvWriter = require('csv-writer').createObjectCsvWriter
-
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 const csvWriter = createCsvWriter({
-  path: 'out.csv',
+  path: "out.csv",
   header: [
-    {id: 'mail', title: 'Mail'},
-    {id: 'points', title: 'Points'},
-  ]
+    { id: "mail", title: "Mail" },
+    { id: "points", title: "Points" },
+  ],
 });
 
-
-
-
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
-const jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json();
+
+app.get("/download", jsonParser, (req, res) => {
+  res.sendFile(__dirname + "/out.csv");
+});
 
 app.post("/getData", jsonParser, function (req, res) {
   surveyResults = `${req.body.Points} ${req.body.Mail}`;
   console.log(surveyResults);
   let results;
   results = [];
-  results.push({ points: req.body.Points, mail: req.body.Mail})
+  results.push({ points: req.body.Points, mail: req.body.Mail });
 
-  csvWriter.writeRecords(results)
-    .then(() => console.log('The CSV file was written successfully'));
-  res.send({ status: 'OK'});
+  csvWriter
+    .writeRecords(results)
+    .then(() => console.log("The CSV file was written successfully"));
+  res.send({ status: "OK" });
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/html/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/html/index.html");
 });
 
-console.log("Сервер запущен...")
+console.log("Сервер запущен...");
 app.listen(80);
--- public/js/vue.js
+++ public/js/vue.js
@@ -1,225 +1,223 @@
 // counter results
 
+const { createConnection } = require("mysql");
+
 app1 = new Vue({
-    el: "#form_radio1",
+  el: "#form_radio1",
 
-    data: {
-        counter: 0,
-    },
-    methods: {
-        yes(){
-            this.counter = 1
-        },
-
-        no(){
-            this.counter = 0
-
-        },
+  data: {
+    counter: 0,
+  },
+  methods: {
+    yes() {
+      this.counter = 1;
     },
 
+    no() {
+      this.counter = 0;
+    },
+  },
 });
 
-
 app2 = new Vue({
-    el: "#form_radio2",
+  el: "#form_radio2",
 
-    data: {
-        counter: 0,
+  data: {
+    counter: 0,
+  },
+  methods: {
+    yes() {
+      this.counter = 1;
     },
-    methods: {
-        yes(){
-            this.counter = 1
-        },
-        no(){
-            this.counter = 0
-        }
+    no() {
+      this.counter = 0;
     },
+  },
 });
 
 app3 = new Vue({
-    el: "#form_radio3",
+  el: "#form_radio3",
 
-    data: {
-        counter: 0,
+  data: {
+    counter: 0,
+  },
+  methods: {
+    yes() {
+      this.counter = 1;
     },
-    methods: {
-        yes(){
-            this.counter = 1
-        },
-        no(){
-            this.counter = 0
-        }
+    no() {
+      this.counter = 0;
     },
+  },
 });
 
 app4 = new Vue({
-    el: "#form_radio4",
+  el: "#form_radio4",
 
-    data: {
-        counter: 0,
+  data: {
+    counter: 0,
+  },
+  methods: {
+    yes() {
+      this.counter = 1;
     },
-    methods: {
-        yes(){
-            this.counter = 1
-        },
-        no(){
-            this.counter = 0
-        }
+    no() {
+      this.counter = 0;
     },
+  },
 });
 
 app5 = new Vue({
-    el: "#form_radio5",
+  el: "#form_radio5",
 
-    data: {
-        counter: 0,
+  data: {
+    counter: 0,
+  },
+  methods: {
+    yes() {
+      this.counter = 1;
     },
-    methods: {
-        yes(){
-            this.counter = 1
-        },
-        no(){
-            this.counter = 0
-        }
+    no() {
+      this.counter = 0;
     },
+  },
 });
 
 app6 = new Vue({
-    el: "#form_radio6",
+  el: "#form_radio6",
 
-    data: {
-        counter: 0,
+  data: {
+    counter: 0,
+  },
+  methods: {
+    yes() {
+      this.counter = 1;
     },
-    methods: {
-        yes(){
-            this.counter = 1
-        },
-        no(){
-            this.counter = 0
-        }
-
+    no() {
+      this.counter = 0;
     },
+  },
 });
 
 result0 = new Vue({
-    el: "#result0",
-    data: {
-        visible: false
-    }
-})
+  el: "#result0",
+  data: {
+    visible: false,
+  },
+});
 
 result1 = new Vue({
-    el: "#result1",
-    data: {
-        visible: false
-    }
-})
+  el: "#result1",
+  data: {
+    visible: false,
+  },
+});
 
 result2 = new Vue({
-    el: "#result2",
-    data: {
-        visible: false
-    }
-})
+  el: "#result2",
+  data: {
+    visible: false,
+  },
+});
 
 result3 = new Vue({
-    el: "#result3",
-    data: {
-        visible: false
-    }
-})
+  el: "#result3",
+  data: {
+    visible: false,
+  },
+});
 
 result4 = new Vue({
-    el: "#result4",
-    data: {
-        visible: false
-    }
-})
+  el: "#result4",
+  data: {
+    visible: false,
+  },
+});
 
 result5 = new Vue({
-    el: "#result5",
-    data: {
-        visible: false
-    }
-})
+  el: "#result5",
+  data: {
+    visible: false,
+  },
+});
 
 result6 = new Vue({
-    el: "#result6",
-    data: {
-        visible: false
-    }
-})
+  el: "#result6",
+  data: {
+    visible: false,
+  },
+});
 
-let results
+let results;
 
 dataSend = new Vue({
-    el: "#btn__send",
-    data: {
+  el: "#btn__send",
+  data: {},
+  methods: {
+    send() {
+      results =
+        app1.counter +
+        app2.counter +
+        app3.counter +
+        app4.counter +
+        app5.counter +
+        app6.counter;
 
+      result0.visible = false;
+      result1.visible = false;
+      result2.visible = false;
+      result3.visible = false;
+      result4.visible = false;
+      result5.visible = false;
+      result6.visible = false;
+
+      if (results == 6) {
+        result6.visible = true;
+      } else if (results == 5) {
+        result5.visible = true;
+      } else if (results == 4) {
+        result4.visible = true;
+      } else if (results == 3) {
+        result3.visible = true;
+      } else if (results == 2) {
+        result2.visible = true;
+      } else if (results == 1) {
+        result1.visible = true;
+      } else if (results == 0) {
+        result0.visible = true;
+      }
+
+      setTimeout(function scrollToResult() {
+        window.scrollTo(0, document.body.scrollHeight);
+      }, 1);
     },
-    methods: {
-
-        send(){
-            results = app1.counter + app2.counter + app3.counter + app4.counter + app5.counter + app6.counter
-
-            result0.visible = false
-            result1.visible = false
-            result2.visible = false
-            result3.visible = false
-            result4.visible = false
-            result5.visible = false
-            result6.visible = false
-
-            if (results == 6) {
-                result6.visible = true
-
-            } else if (results == 5){
-                result5.visible = true
-
-            } else if (results == 4) {
-                result4.visible = true
-
-            } else if (results == 3) {
-                result3.visible = true
-
-            } else if (results == 2) {
-                result2.visible = true
-
-            } else if (results == 1) {
-                result1.visible = true
-
-            } else if (results == 0) {
-                result0.visible = true
-            }
-
-            setTimeout(function scrollToResult() {
-                window.scrollTo(0, document.body.scrollHeight)
-            }, 1)
-
-        },
-
-    },
-
+  },
 });
 
 async function sendData() {
-    results = app1.counter + app2.counter + app3.counter + app4.counter + app5.counter + app6.counter;
+  results =
+    app1.counter +
+    app2.counter +
+    app3.counter +
+    app4.counter +
+    app5.counter +
+    app6.counter;
 
-    let Points = results;
-    let Mail = document.getElementById('mail');
+  let Points = results;
+  let Mail = document.getElementById("mail") + "@croc.ru";
 
-    let data = {
-      Points: Points,
-      Mail: Mail.value
-    };
-
-    console.log(data);
-
-    let response = await fetch('http://80.78.240.74:80/getData', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify(data)
-    });
+  let data = {
+    Points: Points,
+    Mail: Mail.value,
   };
+
+  console.log(data);
+
+  let response = await fetch("http://80.78.240.74:80/getData", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(data),
+  });
+}
